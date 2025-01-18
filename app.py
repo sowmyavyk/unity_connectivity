94a401286ff03f72aa9ff7477c22df954b86adfe
sowmyavyk <vyakaranamsowmya8@gmail.com>
2025-01-18 12:40:02 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["education_db"]
+colleges_collection = db["colleges"]
+
+# Insert sample data (only if the collection is empty)
+if colleges_collection.count_documents({}) == 0:
+    sample_colleges = [
+        {"name": "Harvard University", "location": "Cambridge, MA", "rank": 1},
+        {"name": "Stanford University", "location": "Stanford, CA", "rank": 2},
+        {"name": "MIT", "location": "Cambridge, MA", "rank": 3},
+        {"name": "University of Oxford", "location": "Oxford, UK", "rank": 4},
+    ]
+    colleges_collection.insert_many(sample_colleges)
+
+@app.route("/colleges", methods=["GET"])
+def get_colleges():
+    """Fetch and return the list of colleges."""
+    colleges = list(colleges_collection.find({}, {"_id": 0}))  # Exclude the MongoDB `_id` field
+    return jsonify(colleges)
+
+if __name__ == "__main__":
+    app.run(debug=True)
